3873e9b7baa43fdd6ab9194720de73c54619030f
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-19 13:29:49 +0200

    Challenge#12 Sinatra introducing params

--- app.rb
+++ app.rb
@@ -10,7 +10,13 @@ end
 get '/dogs' do
   'lab'
 end
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo","Oscar","Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p(params)
+  @name = params[:name]
   erb(:index)
 end

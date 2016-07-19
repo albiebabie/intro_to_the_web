7e4d474f334ea450d646d25ca833b300c803c40b
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-19 17:21:15 +0200

    Challenge#14 posted params

--- app.rb
+++ app.rb
@@ -1,22 +1,30 @@
 require 'sinatra'
 
 set :session_secret, 'super secret'
+
 get '/' do
   "Hello World"
 end
+
 get '/secret' do
   'this is a secret'
 end
+
 get '/dogs' do
   'lab'
 end
+
 get '/random-cat' do
   @name = ["Amigo","Oscar","Viking"].sample
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p(params)
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
